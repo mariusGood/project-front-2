d49b15038995623339cee855b6c03b42bc8a921b
mariusGood <mariusgudinas@gmail.com>
2022-05-31 02:48:09 +0300

    created mobile menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import * as S from './Header.style';
 import Navigation from '../Navigation/Navigation';
@@ -12,13 +12,34 @@ const links = [
 ];
 
 const Header = () => {
+  const [block, setBlock] = useState(false);
+
+  const handleClick = () => {
+    block === false ? setBlock(true) : setBlock(false);
+  };
+
   const { logout } = useContext(AuthContext);
   return (
     <S.Header>
       <Navigation links={links} />
-      <Button type='button' color='primary' onClick={logout}>
+      <Button className='logout' type='button' color='primary' onClick={logout}>
         Logout
       </Button>
+      {block && (
+        <S.MenuDiv className='myLinks'>
+          {links.map((e) => (
+            <S.MenuLink key={e.id} to={e.to}>
+              {e.title}
+            </S.MenuLink>
+          ))}
+          <S.Button type='button' onClick={logout}>
+            logout
+          </S.Button>
+        </S.MenuDiv>
+      )}
+      <S.Menu className='menu' onClick={handleClick}>
+        <i className='fa fa-bars'></i>
+      </S.Menu>
     </S.Header>
   );
 };
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Header = styled.header`
@@ -10,4 +11,89 @@ export const Header = styled.header`
   width: 80%;
   background-color: rgba(0, 0, 0, 0.75);
   z-index: 999;
+
+  @media screen and (max-width: 1400px) {
+    left: 7rem;
+  }
+
+  @media screen and (max-width: 1200px) {
+    left: 5rem;
+  }
+
+  @media screen and (max-width: 1000px) {
+    button.sc-bczRLJ {
+      display: none;
+    }
+
+    li {
+      display: none;
+    }
+
+    a.menu {
+      display: block;
+      background-color: #6c11b7;
+      border-radius: 3px;
+      padding: 1rem;
+      border: 1px solid white;
+    }
+
+    a.menu:hover {
+      background-color: white;
+      color: #6c11b7;
+    }
+
+    @media screen and (max-width: 900px) {
+      position: sticky;
+    }
+  }
+`;
+
+export const Menu = styled.a``;
+
+export const MenuDiv = styled.div`
+  @media screen and (max-width: 1000px) {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 1rem;
+    left: 10rem;
+    padding: 2rem;
+    color: white;
+  }
+
+  @media screen and (min-width: 1000px) {
+    display: none;
+  }
+`;
+
+export const MenuLink = styled(Link)`
+  @media screen and (max-width: 1000px) {
+    display: block;
+    color: white;
+    text-decoration: none;
+    padding: 2rem 0;
+
+    :hover {
+      color: #6c11b7;
+    }
+  }
+`;
+
+export const Button = styled.button`
+  background-color: #6c11b7;
+  border-radius: 5px;
+  border: 1px solid white;
+  box-shadow: 0 5px 15px 0 rgb(0 0 0 / 15%);
+  color: white;
+  cursor: pointer;
+  font-weight: bold;
+  padding: 0.75rem 2rem;
+  text-transform: capitalize;
+  margin: 0.5rem 0;
+
+  :hover {
+    background: white;
+    color: #6c11b7;
+    transition: ease 0.2s;
+  }
 `;
